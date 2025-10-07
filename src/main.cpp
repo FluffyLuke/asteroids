4d697b92789d32b381e941c2232c5d5240bfd3c5
FluffyLuke <kacper.lukstaedt@gmail.com>
2025-10-07 20:46:08 +0200

    update: changed to raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,4 @@
-#include "SDL3/SDL.h"
-#include "SDL3/SDL_main.h"
-
 #include "utils.hpp"
-
-constexpr i32 ScreenWidth { 640 };
-constexpr i32 ScreenHeight { 480 };
-
-bool init();
-
-bool loadMedia();
-
-bool close();
 
 i32 main(int argc, char** args) {
     
